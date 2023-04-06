53cdadc4c1bcea0de526fa0a118c34bc1d2f94ce
tnvrravish <vt7@njit.edu>
2023-04-05 20:35:52 -0400

    configured controller to pick min and max values from application.properties using @configuration and @Component.

--- src/main/java/com/vom/microservices/limitsservice/LimitsServiceApplication.java
+++ src/main/java/com/vom/microservices/limitsservice/LimitsServiceApplication.java
@@ -1,5 +1,7 @@
 package com.vom.microservices.limitsservice;
 
+import com.vom.microservices.limitsservice.configuration.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- src/main/java/com/vom/microservices/limitsservice/controller/LimitsController.java
+++ src/main/java/com/vom/microservices/limitsservice/controller/LimitsController.java
@@ -1,14 +1,19 @@
 package com.vom.microservices.limitsservice.controller;
 
 import com.vom.microservices.limitsservice.bean.Limits;
+import com.vom.microservices.limitsservice.configuration.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LimitsController {
 
+    @Autowired
+    private Configuration configuration;
+
     @GetMapping("/limits")
     public Limits retreiveLimitis(){
-        return new Limits(1,1000);
+        return new Limits(configuration.getMinimum(),configuration.getMaximum());
     }
 }
